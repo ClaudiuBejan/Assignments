9f4a1679d411db499179c5f646df4309bd440af4
ClaudiuBejan <78680670+ClaudiuBejan@users.noreply.github.com>
2021-07-23 11:03:16 +0100

    Delete Part 2 PHP directory

--- Part 2 PHP/bankapp/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if($_SERVER['REQUEST_METHOD']=="POST"){
-
-    setcookie('firstname', "k", time()-100, '/');
-    setcookie('lastname', "k", time()-100, '/');
-
-    header("Location: index.php");
-}
